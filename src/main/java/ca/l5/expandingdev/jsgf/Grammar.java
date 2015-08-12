88624b3eda9fcdc6cbaa118e359a6568cd67f590
expandingdev <tylersengia@gmail.com>
2015-08-11 20:23:56 -0400

    Adding Grammar class

---
+++ src/main/java/ca/l5/expandingdev/jsgf/Grammar.java
@@ -0,0 +1,26 @@
+package ca.l5.expandingdev.jsgf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grammar {
+	private List<Rule> rules;
+	public String name;
+	
+	public String compileGrammar() {
+		String f = "#JSGF\n" + "grammar " + name + ";\n";
+		for(int i = 0; i < rules.size(); i++) {
+			f = f.concat(rules.get(i).getRuleString() + "\n");
+		}
+		return f;
+	}
+	
+	public void addRule(Rule r) {
+		rules.add(r);
+	}
+	
+	public Grammar(String n)  {
+		name = n;
+		rules = new ArrayList<Rule>();
+	}
+}
